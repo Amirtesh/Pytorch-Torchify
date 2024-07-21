05788ffeb8d726b55fe2c195bd886e32bee6ef8b
Amirtesh Raghuram <162449183+Amirtesh@users.noreply.github.com>
2024-07-21 20:49:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torchify',
-    version='0.2.0',
+    version='0.3.0',
     description='A Keras-like API for PyTorch with support for image and tabular data',
     author='Amirtesh Raghuram',
     url='https://github.com/Amirtesh/Pytorch-Torchify/tree/main',
